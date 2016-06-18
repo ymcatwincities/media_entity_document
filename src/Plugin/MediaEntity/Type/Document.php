f6910cee295516225b6cd9509ec2a9337ba461a0
Fabien Mercier <fabien@ows.fr>
2016-06-18 18:37:17 +0200

    Avoid an error when field source is not filling

--- src/Plugin/MediaEntity/Type/Document.php
+++ src/Plugin/MediaEntity/Type/Document.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Drupal\media_entity_document\Plugin\MediaEntity\Type;
 
 use Drupal\media_entity\MediaInterface;
@@ -82,16 +83,22 @@ class Document extends MediaTypeBase {
     $source_field = $this->configuration['source_field'];
     /** @var \Drupal\file\FileInterface $file */
     $file = $this->entityTypeManager->getStorage('file')->load($media->{$source_field}->target_id);
-    $mimetype = $file->getMimeType();
-    $mimetype = explode('/', $mimetype);
-    $thumbnail = $this->config->get('icon_base') . "/{$mimetype[0]}-{$mimetype[1]}.png";
 
-    if (!is_file($thumbnail)) {
-      $thumbnail = $this->config->get('icon_base') . "/{$mimetype[1]}.png";
+    if ($file) {
+      $mimetype = $file->getMimeType();
+      $mimetype = explode('/', $mimetype);
+      $thumbnail = $this->config->get('icon_base') . "/{$mimetype[0]}-{$mimetype[1]}.png";
 
       if (!is_file($thumbnail)) {
-        $thumbnail = $this->config->get('icon_base') . '/document.png';
+        $thumbnail = $this->config->get('icon_base') . "/{$mimetype[1]}.png";
+
+        if (!is_file($thumbnail)) {
+          $thumbnail = $this->config->get('icon_base') . '/document.png';
+        }
       }
+    }
+    else {
+      $thumbnail = $this->config->get('icon_base') . '/document.png';
     }
 
     return $thumbnail;
